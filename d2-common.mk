7a6ed37fb3b50b127885d5b18893d8cd6bdff8eb
David Rouleau <Shabbypenguin@gmail.com>
2014-01-12 18:34:41 -0500

    OmniRom intial

--- d2-common.mk
+++ d2-common.mk
@@ -74,6 +74,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+#init.omni.rc replacement
+#PRODUCT_COPY_FILES += \
+#    device/samsung/d2-common/rootdir/etc/init.local.rc:root/init.omni.rc
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8960
@@ -82,7 +86,12 @@ PRODUCT_COPY_FILES += \
     device/samsung/d2-common/gps/gps.conf:system/etc/gps.conf
 
 # Torch
-PRODUCT_PACKAGES += Torch
+PRODUCT_PACKAGES += OmniTorch
+
+# Compcache/Zram support
+#PRODUCT_COPY_FILES += \
+#device/samsung/d2-common/bin/compcache:system/bin/compcache \
+#device/samsung/d2-common/bin/handle_compcache:system/bin/handle_compcache
 
 # Wifi
 PRODUCT_PACKAGES += \
